9aeffaed239b40f57922a6bfa01b26c4ed5e4eea
bigory <igorboldysh@gmail.com>
2023-09-26 17:20:10 +0300

    First commit

---
+++ src/main/java/com/example/apicalculatedistance/ApiCalculateDistanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.apicalculatedistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCalculateDistanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCalculateDistanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apicalculatedistance/ApiCalculateDistanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apicalculatedistance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCalculateDistanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
